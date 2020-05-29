79a4e528ab631846a39b00557a01ddd2e85e7f18
Debalina Halder <debalina.halder@codeclouds.in>
2020-05-29 23:37:54 +0530

    updated login migration

---
+++ database/migrations/2020_05_28_192704_login.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Login extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+        Schema::table('student', function (Blueprint $table) {
+            $table->string('registartion_id');
+            $table->string('password');
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
