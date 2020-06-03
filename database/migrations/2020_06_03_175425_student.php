ae3f225fb9bfb94dda6de20199ca4af39c6c5e88
Debalina Halder <debalina.halder@codeclouds.in>
2020-06-04 00:51:25 +0530

    all committed

--- database/migrations/2020_06_03_175425_student.php
+++ database/migrations/2020_06_03_175425_student.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Login extends Migration
+class Student extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class Login extends Migration
     public function up()
     {
         
-        Schema::table('student', function (Blueprint $table) {
+        Schema::create('student', function (Blueprint $table) {
             $table->increments('id');
             $table->string('reg_id');
             $table->string('name');
