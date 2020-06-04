b0a09a65567aea27ff3f8ce9dd51153378333fa5
Debalina Halder <debalina.halder@codeclouds.in>
2020-06-05 00:47:12 +0530

    no changes, but committed

--- public/js/app.js
+++ public/js/app.js
@@ -50832,8 +50832,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\CISPL-SUMIT\Desktop\Personal\Sumit Git\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\CISPL-SUMIT\Desktop\Personal\Sumit Git\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Debalina\Self-Project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Debalina\Self-Project\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
