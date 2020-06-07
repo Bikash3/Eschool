9b5871cc7d46030e8e6ceefd11d70a8009287a0c
Bikash Sarkar <bikash.sarkar43@gmail.com>
2020-06-08 01:19:13 +0530

    added contain for dashboard

--- resources/js/components/dashboardComponent.vue
+++ resources/js/components/dashboardComponent.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
         <h1>dashboard</h1>
-        <dashboard-header></dashboard-header>
         <dashboard-navmanu></dashboard-navmanu>
-        <dashboard-body></dashboard-body>
-        <dashboard-footer></dashboard-footer>
+        <!--begin::Wrapper-->
+        <div class="d-flex flex-column flex-row-fluid wrapper" id="kt_wrapper">
+            <dashboard-header></dashboard-header>
+            <dashboard-body></dashboard-body>
+            <dashboard-footer></dashboard-footer>
+        </div>
+        
     </div>
 </template>
 
@@ -13,7 +17,15 @@ export default {
 
 }
 </script>
-
-<style>
-
+<style scoped>
+    @import "/public/css/css.css";
+    @import "/public/css/fullcalendar.bundle7a4a.css";
+    @import "/public/css/plugins.bundle7a4a.css";
+    @import "/public/css/prismjs.bundle7a4a.css";
+    @import "/public/css/style.bundle7a4a.css";
+    @import "/public/css/header/light7a4a_base.css";
+    @import "/public/css/header/light7a4a_menu.css";
+    @import "/public/css/dark7a4a_brand.css";
+    @import "/public/css/dark7a4a_aside.css";
+    @import url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css);
 </style>
