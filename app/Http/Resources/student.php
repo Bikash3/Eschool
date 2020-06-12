b7b07d21e4b5f8efe341a8dd93a9606e48ed2c76
Debalina Halder <debalina.halder@codeclouds.in>
2020-06-12 23:21:52 +0530

    api route committed

--- app/Http/Resources/student.php
+++ app/Http/Resources/student.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use App\Model\Student;
+use App\Model\students;
 
 class student extends JsonResource
 {
--- routes/api.php
+++ routes/api.php
@@ -2,26 +2,17 @@
 
 use Illuminate\Http\Request;
 use App\Http\Resources\student as StudentResource;
-use App\Model\student;
+use App\Model\students;
 use App\User;
 
 
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-Route::get('/student', function () {
-    return StudentResource::collection(student::all());
-});
+// Route::post('/student', function () {
+//     return StudentResource::collection(student::all());
+// });
+
+Route::apiResource('/students','dashboardController');
