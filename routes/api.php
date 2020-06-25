0d0d67ee9000c1cc615736ab799f69578c23d270
Bikash Sarkar <bikash.sarkar43@gmail.com>
2020-06-26 01:56:20 +0530

    changed routes for APIs

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,10 @@
 <?php
-
-use Illuminate\Http\Request;
-//use App\Http\Resources\student as StudentResource;
-//use App\Http\Resources\employee as EmployeeResource;
-//use App\Model\students;
-//use App\Model\employees;
-use App\User;
-
-
-
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-// Route::get('/student', function () {
-//     return StudentResource::collection(students::all());
-// });
-
-//Route::apiResource('/students','dashboardController');
-
-// Route::get('student/{id}', function ($id){
-//     return 'students '.$id;
-
-// })->where('name', 'student1');
-
 // Student API Details
-Route::get('/student_all', 'api\student@index');
-Route::get('student_edit/{id}', 'api\student@edit');
-Route::put('student_update/{id}', 'api\student@update');
+Route::get('/student/all', 'api\student@index');
+Route::get('/student/single/{id}', 'api\student@edit');
+Route::put('/student/update/{id}', 'api\student@update');
 
 // Employee API details
-Route::get('/employee_all', 'api\employee@index');
-Route::get('employee_edit/{id}', 'api\employee@edit');
-Route::put('employee_update/{id}', 'api\employee@update');
+Route::get('/employee/all', 'api\employee@index');
+Route::get('/employee/single/{id}', 'api\employee@edit');
+Route::put('/employee/update/{id}', 'api\employee@update');
